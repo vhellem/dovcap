04e60abb4de7a36d276848fab0ca4976cfb5b606
Vegard Hellem <vhellem@gmail.com>
2017-09-27 14:37:49 +0200

    Added workplace component

--- src/main/frontend/app/app.js
+++ src/main/frontend/app/app.js
@@ -2,16 +2,31 @@ import React from 'react';
 import { getModelsFromBackend } from './utlities.js';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedModel: false,
+      modelViews: null
+    };
+  }
   componentWillMount() {
     getModelsFromBackend().then(res => {
       const json = JSON.parse(res.text);
 
       console.log(json);
-
+      this.setState({
+        selectedModel: 0,
+        modelViews: json.modelViewL
+      });
     });
   }
+
   render() {
-    return <h1>React app</h1>;
+    console.log(this.state);
+    if (this.state.selectedModel === 0) {
+      return <h1 modelView={this.state.modelViews[this.state.selectedModel]}>Selected model</h1>;
+    }
+    return <h1>loading</h1>;
   }
 }
 export default App;
