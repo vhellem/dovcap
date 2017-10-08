53540430b0df698153e57cca6056b0df1496056c
Simrayz <simenabelsen@hotmail.no>
2017-10-08 23:24:54 +0200

    Add /getModelNames action to return names of all models currently in the /models directory

--- src/main/java/com/dlizarra/starter/ModelController.java
+++ src/main/java/com/dlizarra/starter/ModelController.java
@@ -1,7 +1,13 @@
 package com.dlizarra.starter;
 
 
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class ModelController {
@@ -12,6 +18,22 @@ public class ModelController {
             Parser parser = new Parser("models/simple.kmv");
 
             return parser.getJson();
+        }
+
+        @CrossOrigin(origins = "http://localhost:9090")
+        @RequestMapping(value="/api/getModelNames", method=RequestMethod.GET)
+        public String getModelNames() {
+          File folder = new File("models");
+          File[] files = folder.listFiles();
+          ArrayList fileNames = new ArrayList();
+
+          for(File file : files) {
+            if (file.isFile()) {
+             fileNames.add(file.getName());
+            }
+          }
+          String json = new Gson().toJson(fileNames);
+          return json;
         }
 
     }
