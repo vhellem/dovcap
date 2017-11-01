91de77a7c78b73e82b7cca907b6ec833163008c4
Vegard Hellem <vhellem@gmail.com>
2017-11-01 13:38:59 +0100

    test: add testing

---
+++ src/main/frontend/app/tests/ModelView.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ModelView from '../ModelView';
+
+const mockModel = {
+  children: [],
+};
+
+const mockRelationships = {
+  relationships: [],
+};
+
+describe('<ModelView />', () => {
+  it('should not change unexpectedly', () => {
+    const tree = renderer
+      .create(
+        <ModelView
+          modelView={mockModel}
+          relationships={mockRelationships}
+          zoom={0}
+          xOffset={5}
+          yOffset={5}
+          width={800}
+          height={800}
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
