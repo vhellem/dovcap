a6454d0ec03c49b0a64cd7ac795245adbe4d2db8
Erik <erikbjorgen@gmail.com>
2017-11-13 15:24:42 +0100

    - Backend testing looks better now.  - It also does some snapshot testing, and the "target json" is saved in    testing/ folder.

--- src/main/java/com/dlizarra/starter/TestParser.java
+++ src/main/java/com/dlizarra/starter/TestParser.java
@@ -1,14 +1,69 @@
 package com.dlizarra.starter;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
+import com.google.gson.*;
+import java.io.*;
 
 public class TestParser {
+    Parser parser;
+
+    @Before
+    public void setUp() {
+        System.out.println("Initialising parser with simple model.");
+        //Parser parser = new Parser("../../../../../../models/simple2.kmv");
+        parser = new Parser("../../../models/simple2.kmv");
+    }
+
     @Test
     public void testInit() {
-      //Parser parser = new Parser("../../../../../../models/simple2.kmv");
-      Parser parser = new Parser("../../../models/simple2.kmv");
-      Assert.assertNotNull(parser);
-      Assert.assertNotNull(parser.getJson());
+        Assert.assertNotNull(parser);
+    }
+
+    @Test
+    public void testGetJson() {
+        String jsonString = parser.getJson();
+        Assert.assertNotNull(jsonString);
+        Assert.assertTrue(validateJson(jsonString));
+        Assert.assertTrue(compareJson(jsonString));
+    }
+
+    private boolean validateJson(String jsonString) {
+        Gson gson = new Gson();
+        try{
+            if(gson.fromJson(jsonString, Object.class) != null){
+                return true;
+            }
+            return false;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+    private boolean compareJson(String json) {
+        char[] actual = json.toCharArray();
+        FileReader in = null;
+
+        try{
+            in = new FileReader("testing/simple_goal.txt");
+
+            int c;
+            int i = 0;
+            while((c = in.read()) != -1) {
+                if(actual[i++] != (char)c){
+                    return false;
+                }
+            }
+        } catch(ArrayIndexOutOfBoundsException aioobe) {
+            try{
+                if(in.read() != -1){
+                    return false;
+                }
+            } catch(Exception e) {
+                return false;
+            }
+        } catch(Exception e){
+            return false;
+        }
+        return true;
     }
 }
